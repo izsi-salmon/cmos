08c587868281cd3754f19cc3624a0b2c4c35f077
izsi-salmon <izsi.salmon@gmail.com>
2019-07-10 15:20:10 +1200

    completed FAQ page - index styles

--- includes/banner.php
+++ includes/banner.php
@@ -1,12 +1,26 @@
 <?php  if(has_post_thumbnail()): ?>
    <div class="banner-image" style="background-image: url(<?php echo esc_url(get_the_post_thumbnail_url()) ?>)">
         <div class="banner-text">
+           <?php 
+                $id= get_the_id();
+                $post = get_post($id); 
+                $theTitle = apply_filters('the_title', $post->post_title); 
+                
+                 if(strlen($theTitle) < 21):
+            ?>
             <h1 class="banner-title">
-                <?php the_title() ?>
+                <?= $theTitle ?>
                 <div class="underline-green underline-50 underline-centered"></div>
             </h1>
+            
+            <?php else: ?>
+            <h1 class="banner-title banner-title-small">
+                <?= $theTitle ?>
+                <div class="underline-green underline-50 underline-centered"></div>
+            </h1>
+            <?php endif; ?>
         </div>
     </div>
 <?php else: ?>
-    <h1 class="section-title"><?php the_title() ?></h1>
+    <h1 class="section-title"><?= $theTitle ?></h1>
 <?php endif; ?>
